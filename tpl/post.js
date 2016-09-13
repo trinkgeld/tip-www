efeced51bdfb6968f36ac513f0a10415dd30403e
Jannis R <mail@jannisr.de>
2016-09-13 09:49:21 +0200

    styling for post

--- tpl/post.js
+++ tpl/post.js
@@ -10,11 +10,14 @@ const time = (date) =>
 		datetime: moment(date).format('YYYY-MM-DD')
 	}, moment(date).locale('de').format('LL'))
 
-const post = (site, page) => base(site, page,
-	  h.div({id: 'post-meta'}, [
+const post = (site, page) => base(
+	site,
+	Object.assign({}, page, {stylesheets: ['/post.css']}),
+	page.content
+	+ h.p({id: 'post-meta'}, [
 		h.span({id: 'post-meta-author'}, page.author),
+		' am ',
 		time(page.date)
-	])
-	+ page.content)
+	]))
 
 module.exports = Object.assign(post, {time})
