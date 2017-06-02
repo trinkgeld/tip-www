33867a1b120cd5f88673d14922b54a232bfa0b1a
Jannis R <mail@jannisr.de>
2017-06-02 15:09:13 +0200

    HTTP only

--- config/default.js
+++ config/default.js
@@ -2,11 +2,7 @@
 
 module.exports = {
 	hostname: 'localhost',
-	port:     3000,
-	httpPort: 3001,
-	key:      '<path to SSL key>',
-	cert:     '<path to SSL cert>',
-	ca:       '<path to SSL CA cert>',
+	port: 3000,
 	backend: {
 		url: 'https://api.tip-me.org/',
 		insecure: false
--- index.js
+++ index.js
@@ -1,24 +1,12 @@
 'use strict'
 
-const spdy = require('spdy')
 const http = require('http')
-const fs = require('fs')
 const config = require('config')
 
 const app = require('./app')
 
-const secure = spdy.createServer({
-	  key:  fs.readFileSync(config.key)
-	, cert: fs.readFileSync(config.cert)
-	, ca:   fs.readFileSync(config.ca)
-}, app)
-secure.listen(config.port, (err) => {
-	if (err) console.error(err)
-	else console.log(`HTTPS server listening on ${config.port}.`)
-})
-
 const insecure = http.createServer(app)
-insecure.listen(config.httpPort, (err) => {
+insecure.listen(config.port, (err) => {
 	if (err) console.error(err)
-	else console.log(`HTTP server listening on ${config.httpPort}.`)
+	else console.log(`HTTP server listening on ${config.port}.`)
 })
